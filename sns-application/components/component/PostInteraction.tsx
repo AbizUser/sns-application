1a523d57f19ce04b52154eb08f01e4742a21be8b
AbizUser <aman.biz.t23@gmail.com>
2024-11-15 07:41:19 +0900

    add postInteractionlogic

--- sns-application/components/component/PostInteraction.tsx
+++ sns-application/components/component/PostInteraction.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
 import { Button } from "@/components/ui/button";
 import { HeartIcon, MessageCircleIcon, Share2Icon } from "./Icons";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 type PostInteractionProps = {
   postId: string;
@@ -13,19 +15,61 @@ const PostInteraction = ({
   initialLikes,
   commentNumber,
 } : PostInteractionProps) => {
+
+  const likeAction = async () => {
+    "use server";
+
+    const { userId } = auth();
+
+    if(!userId) {
+      throw new Error("User is not authenticated");
+    }
+
+    try {
+      const existingLike = await prisma.like.findFirst({
+        where: {
+          postId,
+          userId,
+        }
+      });
+
+      if (existingLike) {
+        await prisma.like.delete({
+          where: {
+            id: existingLike.id,
+          },
+        });
+
+        revalidatePath("/");
+      } else {
+        await prisma.like.create({
+          data: {
+            postId,
+            userId,
+          }
+        })
+        revalidatePath("/");
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className='flex items-center'>
-      <Button variant="ghost" size="icon">
-        <HeartIcon className="h-5 w-5 text-muted-foreground" />
-      </Button>
-      <span className='ml-1 text-destructive'>{initialLikes.length}</span>
-      <Button variant="ghost" size="icon">
-        <MessageCircleIcon className="h-5 w-5 text-muted-foreground" />
-      </Button>
-      <span>{commentNumber}</span>
-      <Button variant="ghost" size="icon">
-          <Share2Icon className="h-5 w-5 text-muted-foreground" />
-      </Button>
+      <form action={likeAction}>
+        <Button variant="ghost" size="icon">
+          <HeartIcon className="h-5 w-5 text-muted-foreground" />
+        </Button>
+        <span className='ml-1 '>{initialLikes.length}</span>
+        <Button variant="ghost" size="icon">
+          <MessageCircleIcon className="h-5 w-5 text-muted-foreground" />
+        </Button>
+        <span>{commentNumber}</span>
+        <Button variant="ghost" size="icon">
+            <Share2Icon className="h-5 w-5 text-muted-foreground" />
+        </Button>
+      </form>
     </div>
   )
 }
