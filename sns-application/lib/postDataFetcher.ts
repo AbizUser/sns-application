4896bdc7ff14e2c5444174c48d7ec6fc47ac5e53
AbizUser <aman.biz.t23@gmail.com>
2024-11-16 21:01:31 +0900

    add profilepage

--- sns-application/lib/postDataFetcher.ts
+++ sns-application/lib/postDataFetcher.ts
@@ -1,42 +1,73 @@
 import prisma from "./prisma";
 
-export async function fetchPosts(userId: string) {
-  //SSG
-  // fetch("api", {force-cache})
-  const following = await prisma.follow.findMany({
-    where: {
-      followerId: userId,
-    },
-    select: {
-      followingId: true,
-    },
-  });
+export async function fetchPosts(userId: string, username?: string) {
+
+  if(username) {
+    return await prisma.post.findMany({
+      where: {
+        author: {
+          name: username,
+        },
+      },
+      include:{
+        author: true,
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            replies: true,
+          }
+        }
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }
+
+  if(!username && userId) {
+    //SSG
+    // fetch("api", {force-cache})
+    const following = await prisma.follow.findMany({
+      where: {
+        followerId: userId,
+      },
+      select: {
+        followingId: true,
+        },
+    });
 
   const followingIds = following.map((f) => f.followingId);
   const ids = [userId, ...followingIds];
-
-  //SSR
-  return await prisma.post.findMany({
-    where: {
-      authorId: {
-        in: ids,
-      },
-    },
-    include: {
-      author: true,
-      likes: {
-        select: {
-          userId: true,
+    //SSR
+    return await prisma.post.findMany({
+      where: {
+        authorId: {
+          in: ids,
         },
       },
-      _count: {
-        select: {
-          replies: true,
+      include: {
+        author: true,
+        likes: {
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            replies: true,
+          }
         }
+      },
+      orderBy: {
+        createdAt: "desc",
       }
-    },
-    orderBy: {
-      createdAt: "desc",
-    }
-  });
+    });
+  }
+
+
+
 }
